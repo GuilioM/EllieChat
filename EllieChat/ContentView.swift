987da326fb9d3c8796864ccb49c67c6cf3df2fdf
GuilioM <GuilioM@users.noreply.github.com>
2023-02-04 17:27:37 -0600

    Initial Commit

---
+++ EllieChat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EllieChat
+//
+//  Created by Guilio Marin on 04/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EllieChat/EllieChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllieChatApp.swift
+//  EllieChat
+//
+//  Created by Guilio Marin on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct EllieChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
